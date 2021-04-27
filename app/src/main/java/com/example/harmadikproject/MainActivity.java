1686b16d5c7ec407cdb5dc75eb57190af911afbd
MMartina020 <matyasmartina020@gmail.com>
2021-04-27 11:42:18 +0200

    Martina 1 rész

---
+++ app/src/main/java/com/example/harmadikproject/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.harmadikproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Gomb = findViewById(R.id.Gomb);
+        ImageView Kep = findViewById(R.id.Kep);
+
+        Gomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultActivity = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(resultActivity);
+            }
+        });
+    }
+}
